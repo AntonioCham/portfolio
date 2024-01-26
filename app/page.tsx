717bd8d3c78c74e2be2dc10bdb841723c93e5c26
Antonio Cham <antoniowlcham2@gmail.com>
2024-01-26 05:50:55 -0500

    add project part

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,128 @@
+"use client"
+
+import { CTASection, aboutSection, projectSection } from "@portfolio/data/personal.json"
+import { ReactTyped } from "react-typed";
+import * as ReactIcons from "react-icons/si";
+import { motion } from "framer-motion"
+import { Metadata } from "next";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default function Home(){
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    type GetIconProps = {
+      icon: string;
+    };
+  
+    const getIcon = ({ icon }: GetIconProps) => {
+      const TagName = ReactIcons[icon as keyof typeof ReactIcons];
+      if (TagName) {
+        return <TagName className="text-4xl" />;
+      } else {
+        console.error(`Invalid icon: ${icon}`);
+        return null;
+      }
+    };
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+    const aboutContent = aboutSection.content.map((info,infoIndex) => {
+        return(
+            <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} key={infoIndex} className="m-12 p-12 border border-black rounded-xl drop-shadow-md shaow-lg leading-8">
+                <div className="text-center">
+                <p className="text-xl">{info.title}</p>
+                <p>{info.company}</p>
+                <p>{info.date}</p>
+                </div>
+                <div className="py-4">
+                <ul className="list-disc text-md leading-8 py-2">
+                {info.description.map((desc, descIndex) => {
+                    return(
+                        <li className="text-md" key={descIndex}>{desc}</li>
+                    )
+                })}
+                </ul>
+                </div>
+                <h4 className="text-lg py-6">Stack</h4>
+                <div className="flex flex-wrap gap-6 justify-center">
+                {info.stack.map((item, itemIndex) => {
+                    return(
+                        <div className="flex flex-col items-center basis-1/12" key={itemIndex}>
+                            {getIcon({icon: item.icon})}
+                            <p className="text-md text-center">{item.name}</p>
+                        </div>
+                    )
+                })}
+                </div>
+            </motion.div>
+        );
+    })
+
+    const projectContent =projectSection.content.map((project, projectIndex) => {
+      return(
+        <>
+        <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} key={projectIndex} className="m-4 mx-auto rounded-xl drop-shadow-md shaow-lg basis-1/3 text-center">
+        <a href={project.githubLink} target="_blank">
+          <Image src={project.picture} alt={project.projectName} width={500} height={500} className="w-full rounded-lg"></Image>
         </a>
+        <p className="text-lg py-4">{project.projectName}</p>
+        </motion.div>
+        </>
+      )
+    })
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return(
+        <main className="bg-gradient-to-t from-gray-300">  
+        <section className="min-h-screen">
+            <div className="text-center p-12 h-screen w-screen flex flex-col justify-center items-center">
+            <div>
+                <motion.div whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                <video className="mx-auto w-80 h-80 rounded-full size-1/6 my-4 bg-gradient-to-b from-gray-200" autoPlay loop muted>
+                        <source src="Antonio.webm" type="video/webm"/>
+                </video>
+                </motion.div>
+                <h2 className="text-6xl py-2">
+                    { CTASection.name }
+                </h2>
+                <p className="text-4xl py-2">
+                    I&apos;m a {" "}
+                    <ReactTyped strings={CTASection.selfwords}
+                    typeSpeed={100}
+                    loop
+                    backSpeed={20}
+                    cursorChar=">"
+                    showCursor={true} />
+                </p>
+                <div className="py-5">
+                    { CTASection.description.map((desc, descIndex) => {
+                        return(
+                            <p key={descIndex} className="text-md leading-8 text-grey-800">{desc}</p>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="flex justify-center text-4xl gap-16">
+                <motion.a whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="text-5xl drop-shadow-md" href="https://www.linkedin.com/in/antoniocham/" target="_blank"><ReactIcons.SiLinkedin /></motion.a>
+                <motion.a whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="text-5xl drop-shadow-md" href="https://github.com/antoniocham" target="_blank"><ReactIcons.SiGithub /></motion.a>
+                <motion.a whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className="text-5xl drop-shadow-md" href="mailto:antoniowlcham2@gmail.com" target="_blank"><ReactIcons.SiGmail /></motion.a>
+            </div>
+            <motion.button className="border-solid rounded-lg border-black border-2 p-4 m-6" whileHover={{scale: 1.1}} whileTap={{scale: 0.9}}>
+                <a href="#" download="AntonioCham_Resume.pdf">My Resume</a>
+            </motion.button>
+            </div>
+        </section>
+        <section className="p-12">
+            <h3 className="text-3xl py-1 text-center">{aboutSection.header}</h3>
+            <div className="lg:flex gap-10">
+                {aboutContent}
+            </div>
+        </section>
+        <section className="p-12">
+            <h3 className="text-3xl py-1 text-center">{projectSection.header}</h3>
+            <div className="flex flex-col gap-10 py-10 items-center lg:flex-row lg:flex-wrap">
+              {projectContent}
+            </div>
+        </section>
+        <section className="p-12 text-center">
+            <p> 2024 © Antonio Cham</p>
+        </section>
+        </main>
+    )
 }
